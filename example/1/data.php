5d2592803e464f8f79a1de10449ed723f90cf5ed
Florian Theimer <f.theimer@seniorbook.de>
2015-11-05 23:02:25 +0100

    Working

---
+++ example/1/data.php
@@ -0,0 +1,6 @@
+<?php
+
+
+sleep(3);
+
+echo '<h1>Loading data for ' . $_GET['i'] . '</h1>';
--- lazy.js
+++ lazy.js
@@ -1,5 +1,20 @@
-$( document ).ready( function() {
-    $('.lazyjs').each( function() {
-        console.log($( this)[0].attributes);
+var LazyUrl;
+
+bla = {"animation": true};
+
+LazyJs(bla);
+
+function LazyJs(arg){
+
+    $( document ).ready( function() {
+        $('.lazyjs').each( function() {
+            var element = $( this );
+            if(arg.animation) $( element ).html('<p class="lazyjsLoading">Loading <span>.</span><span>.</span><span>.</span></p>');
+            LazyUrl = $( this ).attr( 'data-origin');
+            $.ajax({url: LazyUrl, success: function(result){
+                $( element ).html(result);
+            }});
+
+        });
     });
-});
+}
